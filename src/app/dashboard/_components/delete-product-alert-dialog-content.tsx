f84a4fb59b2e11afa5661e97315a9ace4179a348
MarselDokubo <connectmarsel@gmail.com>
2024-11-02 13:43:24 +0100

    fix: delete product bug

---
+++ src/app/dashboard/_components/delete-product-alert-dialog-content.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "~/components/ui/alert-dialog";
+import { toast } from "sonner";
+import { deleteProduct } from "~/server/actions/products";
+import { useTransition } from "react";
+
+export function DeleteProductAlertDialogContent({ id }: { id: string }) {
+  const [isDeletePending, startDeleteTransition] = useTransition();
+
+  return (
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete this
+          product.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction
+          onClick={() => {
+            startDeleteTransition(async () => {
+              const data = await deleteProduct(id);
+              if (data.error) {
+                toast.error(data.message);
+              } else {
+                toast(data.message);
+              }
+            });
+          }}
+          disabled={isDeletePending}
+        >
+          Delete
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  );
+}
