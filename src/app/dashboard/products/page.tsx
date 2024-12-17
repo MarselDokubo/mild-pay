1cfd6a007341f58d1e1d5dccac5446426699161b
MarselDokubo <connectmarsel@gmail.com>
2024-12-17 18:04:30 +0100

    chore(page): Products page

---
+++ src/app/dashboard/products/page.tsx
@@ -0,0 +1,27 @@
+import { auth } from "@clerk/nextjs/server";
+import { getProducts } from "~/server/db/products";
+import { Button } from "~/components/ui/button";
+import Link from "next/link";
+import { PlusIcon } from "lucide-react";
+import { NoProducts } from "../_components/no-products";
+import { ProductGrid } from "../_components/product-grid";
+
+export default async function DashboardPage() {
+  const { userId, redirectToSignIn } = await auth();
+  if (userId == null) return redirectToSignIn();
+  const products = await getProducts(userId, { limit: 6 });
+  if (products.length === 0) return <NoProducts />;
+  return (
+    <>
+      <h1 className="mb-6 text-3xl font-semibold flex justify-between">
+        Products
+        <Button asChild>
+          <Link href="/dashboard/products/new">
+            <PlusIcon className="size-4 mr-2" /> New Product
+          </Link>
+        </Button>
+      </h1>
+      <ProductGrid products={products} />
+    </>
+  );
+}
