a03fcb5742c244c50c3bd0fdec502dc6f2b03391
MarselDokubo <connectmarsel@gmail.com>
2024-12-17 14:41:43 +0100

    chore(page): product detail

--- src/app/dashboard/products/[id]/page.tsx
+++ src/app/dashboard/products/[id]/page.tsx
@@ -1,7 +1,14 @@
-export default function ProductPage() {
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
   return (
     <>
-      <div className=""></div>
+      <div className="">
+        Ahoy!! You're on the product page. The product ID is {id}
+      </div>
     </>
   );
 }
